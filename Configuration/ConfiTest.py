a8131e815b978afa4b422633c9a43d6e96dfefe5
unknown <satishkumarofficial74@gmail.com>
2022-01-28 10:29:30 +0530

    few changes

--- Configuration/ConfiTest.py
+++ Configuration/ConfiTest.py
@@ -1,15 +1,15 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.opera import OperaDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
-
-@pytest.fixture(params=["chrome"])
+@pytest.fixture(params=["firefox"])
 def init_driver(request):
     if request.param == "chrome":
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-    if request.param == "opera":
-        driver = webdriver.Opera(OperaDriverManager().install())
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    if request.param == "firefox":
+        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
     request.cls.driver = driver
     driver.implicitly_wait(100)
     driver.set_page_load_timeout(100)
